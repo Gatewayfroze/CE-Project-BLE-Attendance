be491c17484255eab629525a5dfd934324a85c3b
IX <nitinon556@hotmail.com>
2019-09-27 01:55:54 +0700

    Update UI Login Page

--- ATT-BLE/screens/loginScreen.js
+++ ATT-BLE/screens/loginScreen.js
@@ -1,22 +1,34 @@
 import React from 'react'
-import { View, StyleSheet,Button,Text} from 'react-native'
+import { View, StyleSheet, Text, TextInput } from 'react-native'
+import Button from '../components/button'
+import Colors from '../constants/Colors'
 
 const LoginScreen = props => {
     return (
         <View style={styles.screen}>
             <Text>
-                Sawadee
+                ATT BLE
             </Text>
-            <Button title='Login' onPress={()=> props.navigation.navigate({
-                routeName:'inApp'
-            })}/>
+            <TextInput style={{ height: '3%',backgroundColor:'#f7f7f7', width: '35%', marginVertical: 15, borderBottomColor:Colors.primaryColor ,borderBottomWidth:2}} 
+            placeholder="Username"
+            placeholderTextColor="grey"/>
+            <TextInput style={{ height: '3%',backgroundColor:'#f7f7f7', width: '35%', marginVertical: 15, borderBottomColor:Colors.primaryColor ,borderBottomWidth:2 }} 
+            placeholder="Password"
+            placeholderTextColor="grey"/>
+            <Button style={{ height: '5%', width: '20%' }}
+                click={() => props.navigation.navigate({
+                    routeName: 'inApp'
+                })}>
+                <Text style={{ color: 'white' }}>Login</Text>
+            </Button>
         </View>
     )
 }
-const styles=StyleSheet.create({
-    screen:{
-        flex:1,
-        alignItems:'center'
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 })
 export default LoginScreen
