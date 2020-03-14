14dcbf2c6e676653e56f861634cc63b61f881f74
FaronG <panotsodsri@gmail.com>
2020-03-14 13:17:20 +0700

    createTempTransaction

--- firebaseFunction/functions/transactionfx.js
+++ firebaseFunction/functions/transactionfx.js
@@ -43,6 +43,26 @@ app.post("/createTransaction", (req, res) => {
     });
 });
 
+app.post("/createTempTransaction", (req, res) => {
+  db.collection("transactions")
+    .add({
+      timestamp: req.body.timestamp,
+      schIndex: req.body.schIndex,
+      status: req.body.status,
+      studentUID: req.body.uid,
+      subjectID: req.body.subjectID,
+      uniqueID: req.body.uniqueID
+    })
+    .then( () => {
+      res.end()
+      return
+      
+    })
+    .catch(error => {
+      console.log(error, toString());
+    });
+});
+
 app.post("/getTransactionSub", async (req, res) => {
   await db
     .collection("transactions")
